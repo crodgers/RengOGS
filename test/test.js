17c6548c04e705822fd968a0ef95f96ab03701c0
crodgers <lunchbox.rodgers@gmail.com>
2016-02-04 12:52:08 -0600

    Add test cases
    Test to insure that '/' returns 200
    
    Test to insure that a POST from the login form to '/login' will return
    200 and the request object will have the properties:
    -access_token
    -refresh_token
    -username

--- test/test.js
+++ test/test.js
@@ -1,6 +1,12 @@
 var http_mocks = require('node-mocks-http');
 var expect = require('chai').expect;
 var app_router = require('../app/routes');
+var passport = require('passport');
+
+//need a passport instance to use the router
+passport.initialize();
+initPassport = require('../config/passport/init');
+initPassport(passport);
 
 function buildRes() {
 	return http_mocks.createResponse({eventEmitter: require('events').EventEmitter});
@@ -18,7 +24,30 @@ describe('root route', function() {
 			expect(res.status).to.equal(200);
 		});
 
-		app_router.handle(req, res, function() {
+		app_router(passport).handle(req, res, function() {
+			done();
+		});
+	});
+});
+
+describe('login route', function() {
+	it('responds to /login post', function(done) {
+		var res = buildRes();
+		var req = http_mocks.createRequest({
+			method: "POST",
+			username: "crodgers",
+			password: process.env.TEST_PASSWORD,
+			url: "/login"
+		});
+
+		res.on('end', function() {
+			expect(res.status).to.equal(200);
+			expect(req).to.have.property('username');
+			expect(req).to.have.property('access_token');
+			expect(req).to.have.property('refresh_token');
+		});
+
+		app_router(passport).handle(req, res, function() {
 			done();
 		});
 	});
