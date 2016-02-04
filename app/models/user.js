e57c9ed4145829c2041e7dd7e1d8e7f6d9821961
crodgers <lunchbox.rodgers@gmail.com>
2016-02-04 12:38:39 -0600

    Add attributes to user model
    The app needs to store the OGS access and refresh tokens acquired from
    https://online-go.com/oauth2/access_token

--- app/models/user.js
+++ app/models/user.js
@@ -1,12 +1,13 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt-nodejs');
 
 var schema = mongoose.Schema({
 	username:  String,
 	ogs_id: Number,
+	ogs_access_token: String,
+	ogs_refresh_token: String,
 	notifications: Array,
 	following: Array
-})
+});
 
 module.exports = mongoose.model('Users', schema);
 
