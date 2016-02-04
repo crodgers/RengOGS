e34c835609f9aff2605e1b5b17e775eca70a7b46
crodgers <lunchbox.rodgers@gmail.com>
2016-02-04 12:50:43 -0600

    Change UI to accommodate login
    Add home.ejs which uses a dynamic username.
    
    Change form input in index.ejs to account for the fact that Polymer's
    iron-form won't redirect.
    
    Add functionality in scripts.js to extract data from the paper elements
    and use a hidden form to post (Polymer paper-* elements can't be seen by
    the form).

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,3 +4,17 @@ document.addEventListener('dom-change', function() {
 		document.querySelector("#index_login_dialog").open();
 	});
 });
+
+function submit_hidden_form(event) {
+	console.log(event);
+	var form_username = document.createElement('input');
+	var form_password = document.createElement('input');
+
+	form_username.setAttribute('type', 'hidden');
+	form_password.setAttribute('type', 'hidden');
+
+	form_username.value = document.getElementById('paper_username').value;
+	form_password.value = document.getElementById('paper_password').value;
+
+	document.forms[0].submit();
+}
