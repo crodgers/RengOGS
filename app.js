87b3ff7cc120e07cf48cf78308fbb8334cd6336d
crodgers <lunchbox.rodgers@gmail.com>
2016-02-04 14:22:44 -0600

    Revert "Move bower_components into main directory"
    This reverts commit 06557bf3c81e4256c17a5ad8266bb1f6304e4221.

--- app.js
+++ app.js
@@ -33,8 +33,7 @@ initPassport = require('./config/passport/init');
 initPassport(passport);
 app.use('/', require('./app/routes')(passport));
 
-app.use("/public", express.static(__dirname + "/public"));
-app.use("/bower_components", express.static(__dirname + "/bower_components"));
+app.use(express.static(__dirname + "/public"));
 
 var server = app.listen(process.env.PORT || 5000, function() {
 	console.log('Running on port %s', process.env.PORT || 5000);
