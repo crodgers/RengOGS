9dc644662cf79c40a2821a5707fb903c05ccc230
crodgers <lunchbox.rodgers@gmail.com>
2016-02-07 09:00:09 -0600

    Add authenticated middleware to router
    If the user is not authenticated, redirect all requests to the root page

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,14 @@
 var router = require('express').Router()
 
 var isAuthenticated = function (req, res, next) {
-	// if user is authenticated in the session, call the next() to call the next request handler 
-	// Passport adds this method to request object. A middleware is allowed to add properties to
-	// request and response objects
+	// if user is authenticated, continue to routing
 	if (req.isAuthenticated())
 		return next();
-	// if the user is not authenticated then redirect him to the login page
+	// if the user is not authenticated then redirect to root page
 	res.redirect('/');
 }
+
+router.use(isAuthenticated);
 
 module.exports = function(passport) {
 	router.get('/', function(req, res) {
